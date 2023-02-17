7ddbb2365738f2b11faf01747d2c1308d1ce9918
Montgomery, Christian <cmontgomery@mmi-direct.com>
2023-02-17 08:27:39 -0600

    better

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,43 +1,57 @@
-'use strict';
+"use strict";
 
-const firebase = require('../db');
-const Session = require('../models/session');
+const firebase = require("../db");
+const Session = require("../models/session");
 const firestore = firebase.firestore();
 
 const addSession = async (req, res, next) => {
-    try {
-        const data = {
-            id: 1,
-            user: "Bob",
-            url: "google.com",
-            duration: 100
-        };
-        await firestore.collection('sessions').doc().set(data);
-        res.send('Record saved successfuly');
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-}
+  try {
+    const data = {
+      id: 1,
+      user: "Bob",
+      url: "google.com",
+      duration: 100,
+    };
+    await firestore.collection("sessions").doc().set(data);
+    res.send("Record saved successfuly");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 const addUniqueSession = async (req, res, next) => {
-    try {
+  try {
+    let sessions = await sessions.get();
+    let changedSession = false;
+    await sessions.forEach((doc) => {
+      if (sessions.data().session === req.query.session) {
+        doc.update({
+          end: req.query.time,
+          user: req.query.user,
+          end: req.query.end,
+          url: req.query.url,
+          session: req.query.session,
+        });
+        changedSession = true;
+      }
+    });
+    if(!changedSession) {
         let data = {
-            start: req.query.start,
-            end: req.query.end,
-            user: req.query.user,
-            end: req.query.end,
-            url: req.query.url,
-            session: req.query.session
-        }
-        await firestore.collection('sessions').doc().set(data);
-        res.send('Record saved successfuly');
-    } catch (error) {
-        res.status(400).send(error.message);
+        start: req.query.time,
+        end: req.query.time,
+        user: req.query.user,
+        url: req.query.url,
+        session: req.query.session,
+        };
+        await firestore.collection("sessions").doc().set(data);
     }
-}
-
+    res.send("Record saved successfuly");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 module.exports = {
-    addSession,
-    addUniqueSession
-}
+  addSession,
+  addUniqueSession,
+};
