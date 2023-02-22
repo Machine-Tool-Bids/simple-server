6026a439cfe2aa5743c6c8417b23efb8dff5676a
--replace-all <--replace-all>
2023-02-22 09:39:57 -0600

    session

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -36,8 +36,27 @@ const addUniqueSession = async (req, res, next) => {
     }
 }
 
+const addUniqueSession2 = async (req, res, next) => {
+    try {
+        let data = {
+            start: req.query.start,
+            end: req.query.end,
+            user: req.query.user,
+            end: req.query.end,
+            url: req.query.url,
+            session: req.query.session
+        }
+        await firestore.collection('sessions2').doc().set(data);
+        res.send('Record saved successfuly');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+
 
 module.exports = {
     addSession,
-    addUniqueSession
+    addUniqueSession,
+    addUniqueSession2
 }
--- routes/session-routes.js
+++ routes/session-routes.js
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.get('/add', addUniqueSession);
 router.post('/add', addUniqueSession);
+router.post('/add2', addUniqueSession2);
 router.get('/check/:url', async (req, res) => {
     const url = decodeURI(req.params.url);
     try {
@@ -32,6 +33,42 @@ router.get('/check/:url', async (req, res) => {
                 }
             });
             res.send(mode(pages));
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get('/check2/:url', async (req, res) => {
+    const url = decodeURI(req.params.url);
+    try {
+        const sessions = await firestore.collection('sessions2');
+        const data = await sessions.get();
+        let sessionsArray = [];
+        let pages = [];
+        if(data.empty) {
+            res.status(404).send('No session record found');
+        }else {
+            data.forEach(doc => {
+                if(doc.data().url === url) {
+                    sessionsArray.push(doc.data().session);
+                }
+            });
+            data.forEach(doc => {
+                if(sessionsArray.includes(doc.data().session) && doc.data().url !== url) {
+                    pages.push(doc.data().url)
+                }
+            });
+            let first_common = mode(pages);
+            pages = pages.filter(function(x) {
+                return x !== first_common;
+            });
+            let second_common = mode(pages);
+            pages = pages.filter(function(x) {
+                return x !== second_common;
+            });
+            let third_common = mode(pages);
+            res.send([first_common, second_common, third_common])
         }
     } catch (error) {
         res.status(400).send(error.message);
