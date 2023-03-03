f912fc0ea6bbf872cc52e64c39345880afb68ecc
--replace-all <--replace-all>
2023-03-03 08:07:48 -0600

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const studentRoutes = require('./routes/student-routes');
 const sessionRoutes = require('./routes/session-routes');
-
+var sql = require("mssql");
 const app = express();
 
 app.use(express.json());
@@ -14,6 +14,36 @@ app.use(bodyParser.json());
 
 app.use('/api', studentRoutes.routes);
 app.use('/session', sessionRoutes.routes);
+
+app.get('/', function (req, res) {
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'mypassword',
+        server: 'localhost', 
+        database: 'SchoolDB' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from Student', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
 
 
 
